f7a4bdd30fa67253c8904e15a0bb73805b6c7664
root <soheelsaig0966@gmail.com>
2020-02-05 15:12:54 +0100

    Updates

---
+++ precache-manifest.9998aa154b5f0fed47bd220906e6bb6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4702981fd3b9fb6467f5eb802baaadd0",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "69b28d6734c69b931492",
+    "url": "/todoApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "540d3be12b212bacc1f3",
+    "url": "/todoApp/static/js/2.79456264.chunk.js"
+  },
+  {
+    "revision": "69b28d6734c69b931492",
+    "url": "/todoApp/static/js/main.b929200f.chunk.js"
+  },
+  {
+    "revision": "6ecb44290f83f94f01a3",
+    "url": "/todoApp/static/js/runtime-main.3dfde02a.js"
+  }
+]);
